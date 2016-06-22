c1786344d98760645c42a31c62b0b24b43dbeb57
qr0wproject <fclaeys.project@gmail.com>
2016-06-22 14:57:37 +0200

    add cell

--- Training/ViewController.swift
+++ Training/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style:UITableViewCellStyle.Subtitle, reuseIdentifier: "MaCelluleDeTest")
+
+        cell.textLabel?.text = "Ligne #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Sous-titre #\(indexPath.row)"
+
+        return cell
+    }
 
 }
 
